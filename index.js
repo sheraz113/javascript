423b9e5cc86760d06204b4dfbe810a4462e52212
sheraz113 <sheraz.bmj11@gmail.com>
2023-06-09 12:40:32 +0500

    pormt and assingment and Arry

---
+++ index.js
@@ -0,0 +1,45 @@
+// Promt collect data from user 
+// let name = prompt("please enter the name");
+// let fname = prompt("please enter the fname");
+// let phone = prompt("please enter the phone");
+// let email = prompt("please enter the email");
+
+// document.write("Name"+name+"<br>");
+// document.write("father Name"+fname+"<br>");
+// document.write("phone"+phone+"<br>");
+// document.write("Email"+email+"<br>");
+
+//Calculator
+// num = prompt("first number");
+// num2 = prompt("second number");
+
+// mult = num * num2;
+
+// document.write(mult);
+
+//First assingment of javascripte
+// chiken = prompt("The price of chiken is 720 per KG how much do you neend in Grams");
+// mult = 1000/720
+
+// div = chiken * mult;
+
+// document.write(div+"Gram");
+
+//Second Assingment of Javascripte
+// chiken = prompt(" Set the price of chiken in RS");
+// need = prompt("How much do you need in RS");
+
+// grams = 1000
+
+// price = grams*need/chiken
+
+// document.write(price +"Grams");
+
+
+// ARRY
+
+// let students = [prompt("enter id"), prompt("enter id"), prompt("enter id"), prompt("enter id"), prompt("enter id")];
+ 
+// let user = prompt("enter index number you want to print");
+
+// document.write(students[user]);
